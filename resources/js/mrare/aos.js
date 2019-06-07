b61ab6be089bc33d64f7b367ee92c5d9e1c73608
Miika <miika.pulkkinen1@outlook.com>
2019-06-07 19:57:33 +0300

    install theme

---
+++ resources/js/mrare/aos.js
@@ -0,0 +1,11 @@
+//
+//
+// aos.js
+//
+// Initialises the animate on scroll plugin
+
+import AOS from 'aos';
+
+AOS.init({
+    once: true,
+});
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <link rel="stylesheet" href="{{ asset('leap/assets/css/theme.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('leap/assets/css/theme-saas-trend.css') }}">
+    <link rel="stylesheet" href="{{ asset('leap/assets/css/loaders/loader-typing.css') }}">
+    <link rel="preload" as="font" href="leap/assets/fonts/Inter-UI-upright.var.woff2" type="font/woff2"
+        crossorigin="anonymous">
+    <link rel="preload" as="font" href="leap/assets/fonts/Inter-UI.var.woff2" type="font/woff2" crossorigin="anonymous">
+    <title>APEXHQ</title>
+</head>
+
+<body>
+    <div id="apexhq">
+        @include('partials.header')
+        <div class="main">
+            @yield('content')
+        </div>
+        @include('partials.footer')
+    </div>
+
+    {{-- <script src="{{ asset('/leap/assets/js/bootstrap.js') }}"></script>
+    <script src="{{ asset('/leap/assets/js/theme.js') }}"></script> --}}
+
+    {{-- here --}}
+
+</body>
+
+</html>
